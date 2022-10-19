d297d4ab9ece3cef9757d46660ee267b4a58280e
Suu Kirinus Nogueira <50893598+Suu021@users.noreply.github.com>
2022-10-19 16:14:05 -0300

    ajustes, adição de views e rotas

--- LabMakerSite/home/urls.py
+++ LabMakerSite/home/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home, name='bem-vindos'),
+    path('about/', views.about, name='quem-somos'),
+    path('projects/', views.projects, name='projetos'),
+    path('activities/', views.activities, name='atividades'),
+    path('courses/', views.courses, name='cursos'),
+    path('contact/', views.contact, name='contato')
 ]
--- LabMakerSite/home/views.py
+++ LabMakerSite/home/views.py
@@ -1,31 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 def home(req):
     return render(req, './home/index.html')
 
+def about(req):
+    return render(req, './about/about.html')
 
-# def taskList(req):
-#     tasks = Task.objects.all().order_by('-created_at')
-#     return render(req, './tasks/list.html', {'tasks': tasks})
+def projects(req):
+    return render(req, './projects/projects.html')
 
-# def taskView(req, id):
-#     task = get_object_or_404(Task, pk=id)
-#     return render(req, './tasks/task.html', {'task': task})
+def courses(req):
+    return render(req, './courses/courses.html')
 
-# def yourName(req, name):
-#     return render(req, './tasks/yourname.html', {'name': name})
+def activities(req):
+    return render(req, './activities/activities.html')
 
-# def newTask(req):
-#     if req.method == 'POST':
-#         form = TaskForm(req.POST)
-
-#         if form.is_valid():
-#             task = form.save(commit=False)
-#             task.done = 'doing'
-#             task.save()
-#             return redirect('/task-list')
-#     else:
-#         form = TaskForm()
-#         return render(req, './tasks/addtask.html', {'form': form})
+def contact(req):
+    return render(req, './contact/contact.html')
